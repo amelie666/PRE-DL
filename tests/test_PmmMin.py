ac78cdf357c642ba6828471d52395c7e136044b5
shinetek-hm <abcdefskylight@gmail.com>
2020-07-23 17:11:15 +0800

    pmm work

--- tests/test_PmmMin.py
+++ tests/test_PmmMin.py
@@ -1,21 +1,29 @@
 import os
 import unittest
 
-from pre_dl.datasets.PmmMin import PmmMin
+from pre_dl.datasets.PmmMin import PmmMin, PmmMinTimeRange
 
 
 class TestPmmMin(unittest.TestCase):
 
     def test_PmmMinSave(self):
-        cursor_datetime = '20190101000000'
+        cursor_datetime = '20200722090000'
         cursor_fpath = 'tmp/%s_PMM.csv' % cursor_datetime
         pm = PmmMin()
         pm.fetch(cursor_datetime)
         pm.save(cursor_fpath)
         self.assertTrue(os.path.exists(cursor_fpath))
 
+    def test_PmmMinTimeRangeSave(self):
+        range_datetime = '[20200722090000,20200722091000]'
+        cursor_fpath = 'tmp/%s_PMM.csv' % range_datetime
+        pm = PmmMinTimeRange()
+        pm.fetch(range_datetime)
+        pm.save(cursor_fpath)
+        self.assertTrue(os.path.exists(cursor_fpath))
+
     def test_PmmMinPlot(self):
-        cursor_datetime = '20190101000000'
+        cursor_datetime = '20200722090000'
         pm = PmmMin()
         pm.fetch(cursor_datetime)
         pm.plot()
