8f755559941bcd03df0f38c8fd2d37e305875c52
shinetek-hm <465784936@qq.com>
2020-07-24 10:33:38 +0000

    add get time for datagenerator

--- tests/test_datagenerator.py
+++ tests/test_datagenerator.py
@@ -8,7 +8,7 @@ class TestDatasets(unittest.TestCase):
     
     def test_PRENetPlot(self):
         nn_data_dir = r"/hxqtmp/DPLearning/hm/data/PRE"
-        train_gen, valid_gen = data_generator(nn_data_dir, 16)
+        train_gen, valid_gen, test_gen = data_generator(nn_data_dir, 1)
         x,  y = train_gen[0]
         for key in x.keys():
             print(key, x[key].shape)
